2a6e0ed455854ad8ce593c0d8df6297ac5cd5afe
merttechnology@gmail.com <merttechnology@gmail.com>
2019-06-16 01:16:15 -0700

    ilk bolum tamam

--- swiftForever/ViewController.swift
+++ swiftForever/ViewController.swift
@@ -9,12 +9,45 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let cellID = "cellID"
+    
+    lazy var tableview : UITableView = {
+        let tableView = UITableView()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellID)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return tableView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+        view.addSubview(tableview)
+        
+        let constraints = [
+        tableview.topAnchor.constraint(equalTo: view.topAnchor),
+        tableview.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+        tableview.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+        tableview.bottomAnchor.constraint(equalTo: view.bottomAnchor)]
+        
+        NSLayoutConstraint.activate(constraints)
+        
+        dene()
     }
-
-
 }
-
+extension ViewController:UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for:indexPath)
+        
+        cell.textLabel?.text = "furkan"
+        
+        return cell
+    }
+}
---
+++ swiftForever/deneme.swift
@@ -0,0 +1,13 @@
+//
+//  deneme.swift
+//  swiftForever
+//
+//  Created by Zeynep Seyhan on 6/16/19.
+//  Copyright © 2019 farukgozoglu. All rights reserved.
+//
+
+import Foundation
+
+func dene() {
+    print("merhaba dunya")
+}
